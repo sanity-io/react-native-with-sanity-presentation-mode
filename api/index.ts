2f50d36ec84abfe15fb87de3f6f30ccf5726e4a3
Johnny Povolny <johnny.povolny@sanity.io>
2025-06-05 14:27:31 -0700

    Lunch break

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,5 @@
-import { join } from "path";
 import { createRequestHandler } from "@expo/server/adapter/vercel";
-// import { SANITY_STUDIO_URL } from "@/constants";
 
-// Pre-compile the Expo server bundle once per cold start
-export default createRequestHandler({
-    build: join(process.cwd(), 'dist/server'),
-});
-
-// // Vercel entry point
-// export default function handler(req: any, res: any) {
-//   // 🔑 Set or overwrite CSP *before* the Expo handler runs
-//   res.setHeader(
-//     "Content-Security-Policy",
-//     `frame-ancestors 'self' ${SANITY_STUDIO_URL}`
-//   );
-
-//   // Delegate to Expo Router’s dispatcher (routes to /app/api/** code)
-//   return handle(req, res);
-// }
+module.exports = createRequestHandler({
+    build: require('path').join(__dirname, '../dist/server'),
+  });
--- app/api/validate/index+api.ts
+++ app/api/validate/index+api.ts
@@ -9,24 +9,44 @@ import { createSanityClient } from "@/data/client";
 import { validatePreviewUrl } from "@sanity/preview-url-secret";
 
 export async function POST(request: Request) {
+  try {
+    console.log("validate request", request); 
     const { PRIVATE_SANITY_VIEWER_TOKEN: token } = process.env;
     const { secret, pathname, perspective } = await request.json();
 
+    console.log("Secret is present?", secret); 
+
     if (!secret) {
         throw new Error("Preview mode missing token");
-      }
+    }
+
+    console.log("Secret is present?", secret); 
   
     const clientWithToken = createSanityClient({ token });
+    
+    console.log("Client and token found?:",{client: !!clientWithToken, token: !!token} ); 
+
     const url = `${BASE_URL}/${pathname}?sanity-preview-secret=${secret}&sanity-preview-perspective=${perspective}&sanity-preview-pathname=${pathname}`      
+
+    console.log("URL is:", url); 
 
     const { isValid, redirectTo = "/" } = await validatePreviewUrl(
         clientWithToken,
         url     
       );
 
+    console.log("isValid is:", isValid); 
+
     if(!isValid) {
+      console.log("Unauthorized/invalid"); 
       return new Response(JSON.stringify({error: 'Unauthorized'}), {status: 401})
     }
 
+    console.log("Response is:", { isValid, redirectTo, token }); 
+
     return Response.json({ isValid, redirectTo, token });
+  } catch (error) {
+    console.error("Error in validate request:", error); 
+    return new Response(JSON.stringify({error: 'Internal Server Error'}), {status: 500})
   }
+}
