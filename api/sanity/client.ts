21817b0fe8248c2b5c78774dc03aaf18613280d9
Johnny Povolny <johnny.povolny@sanity.io>
2025-06-05 18:33:10 -0700

    WIP

--- api/sanity/client.ts
+++ api/sanity/client.ts
@@ -1,10 +1,10 @@
 // client.ts
-import { SANITY_DATASET, SANITY_PROJECT_ID, SANITY_STUDIO_URL } from "../constants";
 import { ClientPerspective, createClient } from "@sanity/client";
+import { SANITY_DATASET, SANITY_PROJECT_ID } from "../constants";
 
 export const createSanityClient = (config: {token?: string, perspective?: ClientPerspective} = { perspective: 'published' }) => createClient({
-    projectId: SANITY_PROJECT_ID,
-    dataset: SANITY_DATASET,
+    projectId: SANITY_PROJECT_ID || '',
+    dataset: SANITY_DATASET || '',
     useCdn: true,
     apiVersion: '2025-05-30',
     ...config,
--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 const path = require('path');
+
 const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
