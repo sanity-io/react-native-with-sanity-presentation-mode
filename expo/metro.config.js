88915dae7fe093e7a6bc854a2fbd16f3c5c69764
Johnny Povolny <johnny.povolny@sanity.io>
2025-06-06 10:09:48 -0700

    API and expo both start, API still having issue with vercel TS vs locally installed TS

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -3,6 +3,18 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
 config.resolver.extraNodeModules = {
   ...config.resolver.extraNodeModules,
   events: path.resolve(__dirname, 'node_modules/events'),
