ad2ae77ab94c05c31551b321c0d8fc9e052bd013
Johnny Povolny <johnny.povolny@sanity.io>
2025-06-06 09:27:53 -0700

    Remove unused lib

--- api/lib/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BASE_URL = exports.SANITY_STUDIO_URL = exports.SANITY_DATASET = exports.SANITY_PROJECT_ID = void 0;
-exports.SANITY_PROJECT_ID = process.env.EXPO_PUBLIC_SANITY_PROJECT_ID;
-exports.SANITY_DATASET = process.env.EXPO_PUBLIC_SANITY_DATASET;
-exports.SANITY_STUDIO_URL = process.env.EXPO_PUBLIC_SANITY_STUDIO_URL;
-exports.BASE_URL = process.env.EXPO_PUBLIC_BASE_URL || 'http://localhost:8081';
